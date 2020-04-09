11851261ead1758e3372a0d08506326df5f6adf0
jhk <john3.16@snu.ac.kr>
2020-04-09 11:18:28 +0900

    Planner

--- src/custom_controller.cpp
+++ src/custom_controller.cpp
@@ -50,39 +50,7 @@ void CustomController::computeSlow()
     }
     else if(tc.mode == 11)
     {
-                        //task controller for mode 11 ....
-        int walkingHz = 1000;
-        std::chrono::microseconds cycletime(walkingHz);
-        std::chrono::microseconds cycletime1(0);
-        std::chrono::duration<double> elapsed_seconds1 = cycletime-cycletime1;
-        
-        if(command_init == true)
-        {   
-            cycle_count = 0;
-            command_init = false;
-            wkc_.setRobotStateInitialize();
-            wkc_.getUiWalkingParameter(walkingHz, tc.ik_mode, tc.walking_pattern, tc.foot_step_dir, tc.target_x, tc.target_y, tc.target_z, tc.theta, tc.height, tc.step_length_x, tc.step_length_y, tc.dob, rd_);
-            t_begin = std::chrono::high_resolution_clock::now();
-        }
 
-        std::chrono::high_resolution_clock::time_point t_now = std::chrono::high_resolution_clock::now();   
-        std::chrono::duration<double> elapsed_seconds2 = t_now - (t_begin+cycletime*(cycle_count));
-
-        wkc_.walkingCompute(rd_);
-
-        if((elapsed_seconds2.count()>elapsed_seconds1.count()))
-        {
-            cycle_count++;
-        }
-
-        for(int i = 0; i < 12; i++)
-        {
-            ControlVal_(i) = wkc_.desired_leg_q(i);
-        }
-        for(int i = 12; i<MODEL_DOF; i++)
-        {
-            ControlVal_(i) = rd_.q_init_(i);
-        }
     }  
 }
 
@@ -93,5 +61,35 @@ void CustomController::computeFast()
     }
     else if (tc.mode == 11)
     {
+    }
+}
+
+void CustomController::computePlanner()
+{
+    if(tc.mode == 11)
+    {
+        //task controller for mode 11 ....
+        int walkingHz = 1000;  
+        std::chrono::high_resolution_clock::time_point t_finish;
+        
+        if(command_init == true)
+        {   
+            cycle_count = 0;
+            command_init = false;
+            wkc_.setRobotStateInitialize();
+            wkc_.getUiWalkingParameter(walkingHz, tc.ik_mode, tc.walking_pattern, tc.foot_step_dir, tc.target_x, tc.target_y, tc.target_z, tc.theta, tc.height, tc.step_length_x, tc.step_length_y, tc.dob, rd_);
+            t_begin = std::chrono::high_resolution_clock::now();
+        }
+       
+        wkc_.walkingCompute(rd_);
+
+        for(int i = 0; i < 12; i++)
+        {
+            ControlVal_(i) = wkc_.desired_leg_q(i);
+        }
+        for(int i = 12; i<MODEL_DOF; i++)
+        {
+            ControlVal_(i) = rd_.q_init_(i);
+        }
     }
 }
